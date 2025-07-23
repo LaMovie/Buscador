ab6f4f117e14f4e91b8a4634df246470bf982ca8
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-07-23 15:36:01 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -55,7 +55,7 @@ var Lista1 = [
   {name: "🍿M3gan 2.0", url: "https://drive.google.com/file/d/1taNBm3YY5yRanQpU0xZGgD1UP2Nk_n_x/view?usp=drivesdk"}, 
   {name: "🍿Palm Springs", url: "https://drive.google.com/file/d/14G22DcbsEi3eYMUcXjhhatk2t4p2Aggq/view?usp=drivesdk"}, 
   {name: "🍿Criatura Voraz", url: "https://drive.google.com/file/d/11dp_uM5fdtHvCx-LWwgiJHljKLnQirkI/view?usp=drivesdk"}, 
-  {name: "🍿", url: ""}, 
+  {name: "🍿Traela De Vuelta", url: "https://drive.google.com/file/d/11K3Ab2Ty2S4r3DaEOnbeKajjvAmku3Bn/view?usp=drivesdk"}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
