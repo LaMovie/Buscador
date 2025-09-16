f1dc549cea1c5ed4456881da24923a35b1187c7e
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-09-15 22:23:19 -0300

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -64,7 +64,7 @@ var Lista1 = [
   {name: "🍿Mision Imposible 8", url: "https://drive.google.com/file/d/1dljw3aFv_VxR9oxRa5hHcGnB2QE18BTq/view?usp=drivesdk"}, 
   {name: "🍿Los Niños De Masameer", url: "https://drive.google.com/file/d/1fFe-YuIwnaECIiDrY4CRQMYsRpKb-V5B/view?usp=drivesdk"}, 
   {name: "🍿La Hora De La Desaparicion", url: ""}, 
-  {name: "🍿Nobody 2", url: ""}, 
+  {name: "🍿Nobody 2", url: "https://drive.google.com/file/d/1-q2Iul2Oq3CfpXjhS5RAGqhi7PhHwoSe/view?usp=drivesdk"}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
