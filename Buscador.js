e92debb43c2147b4ac910988f8b62c64c0b281f4
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-04-29 22:05:10 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -80,7 +80,7 @@ var Lista1 = [
   {name: "🍿The Boys 5", url: "https://drive.google.com/drive/folders/16vlUiz3pcufnHT6URc9xeIKFfLQNyWih"}, 
   {name: "🍿Whistle", url: "https://drive.google.com/file/d/1sRwwZ8dAc2eesMqahYbWBxnd2XQt2ed-/view?usp=drivesdk"}, 
   {name: "🍿From 4", url: "https://drive.google.com/drive/folders/1MZSokk-ewcU0zkCG8MdZLKQdSidKkpE3"}, 
-  {name: "🍿", url: ""}, 
+  {name: "🍿They Will Kill You", url: "https://drive.google.com/file/d/1pWvuis59ElwxQU5ZXpkljQsnF9KwhtAk/view?usp=drivesdk"}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
