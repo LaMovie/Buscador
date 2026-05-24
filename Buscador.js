2bb50cce5d2b30bf50da00ebc4bae6c786fb5fe5
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-05-24 10:13:17 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -87,7 +87,7 @@ var Lista1 = [
   {name: "🍿Boda Sangrienta 2", url: "https://drive.google.com/file/d/17yFvMC47tWYbfn94PIGxZBMDnyHIbFjV/view?usp=drivesdk"}, 
   {name: "🍿Citadel 2", url: "https://drive.google.com/drive/folders/1DWmRKcb22Q_qtUdfTjxYirIWQQ1UWrmY"}, 
   {name: "🍿Debris", url: "https://drive.google.com/drive/folders/12fTU1ZYr4M_f9l7Uhk8ODf2MeHRineDa"}, 
-  {name: "🍿", url: ""}, 
+  {name: "🍿La Posesion De La Momia", url: "https://drive.google.com/file/d/1vfwzJu3qzWx1x5rN0H3fEPnBPcc7MxAN/view?usp=drivesdk"}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
