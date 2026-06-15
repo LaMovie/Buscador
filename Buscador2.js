dce37113121725e60b1d64725740bc5c043878d9
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-06-15 01:44:52 -0400

    Add files via upload

--- Buscador2.js
+++ Buscador2.js
@@ -520,7 +520,11 @@ var Lista2 = [
     { NAME: "🍿Mas Dura Sera La Caida", URL: "https://drive.google.com/file/d/1YN3y0cDubKVSZGNykXrIsVyf1fRALOBP/view?usp=drivesdk" },
     { NAME: "🍿Venom 3", URL: "https://drive.google.com/file/d/1ZjWemm3wMKkjs4k3YSVb2F1QeVUMYppi/view?usp=drivesdk" },
     { NAME: "🍿Un Rey En La Habana", URL: "https://drive.google.com/file/d/1liAuNkkrDf-eUm0vcCau_tspejvG0LfT/view?usp=drivesdk"},
-    { NAME: "🍿Agentes Del Desorden", URL: "https://drive.google.com/file/d/10j-eMI6Z1bBW3gdU8rk9gerV9e4pe6S4/view?usp=drivesdk"}
+    { NAME: "🍿Agentes Del Desorden", URL: "https://drive.google.com/file/d/10j-eMI6Z1bBW3gdU8rk9gerV9e4pe6S4/view?usp=drivesdk"}, 
+    { NAME: "🌐OBSESION", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}, 
+    { NAME: "🌐MORTAL KOMBAT 2", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}, 
+    { NAME: "🌐AMOS DEL UNIVERSO", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}, 
+    { NAME: "🌐BACKROOMS", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}
 ];
 
 // Selecciona la lista existente
