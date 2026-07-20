18b7fe32a9cd09cddb495ef0f33dabe1578fd5d4
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-20 10:41:44 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -18,7 +18,6 @@ var Lista1 = [
   {name: "📺SeeKee tv", url: "https://h5.swplayer.com"},
   {name: "🍿Constantine", url: "https://drive.google.com/file/d/185RuQOH2eZerthlAMzg_-1SzPB5yOGvn/view?usp=drivesdk"},
   {name: "🍿Invencible 3", url: "https://drive.google.com/drive/folders/1AM2MxJzkCanL7iYXJQLrQWHmdT7A-pPn"},
-  {name: "📺Sport tv", url: "https://lamovie.github.io/CONTENIDO/SPORT-TV"},
   {name: "🍿La Mujer En Las Sombras", url: "https://drive.google.com/file/d/1U_91j6dWT23QHnhca5wm8tP_ZnYATw0T/view?usp=drivesdk"},
   {name: "🍿911", url: "https://drive.google.com/file/d/1lqTiLDezenKB_6ZKfYO1MX22hLkBA-sH/view?usp=drivesdk"},
   {name: "🍿Tierra Plana", url: "https://drive.google.com/file/d/18-Gw04kF5JE6vBN0b2EKiQDKUstOQp8r/view?usp=drivesdk"},
