04d6c5a176031233e3ca161eaa2e14d8c391252d
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-30 02:16:05 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -104,7 +104,7 @@ var Lista1 = [
   {name: "🍿Pixeles", url: "https://www.dropbox.com/scl/fi/9jfi8tsvwtr9rp8xoufrk/PIXELES-2015.mp4?rlkey=5zlz3snspg02y69om7er6dm44&st=hed4tvz8&dl=0"}, 
   {name: "🍿Tetris", url: "https://drive.google.com/file/d/13T4HlhA53qS4drzO-mE3dvhJ1I2ko6L9/view?usp=drivesdk"}, 
   {name: "🍿El Laberinto Del Fauno", url: "https://www.dropbox.com/scl/fi/63a9cshtjhzjnweh0poy3/EL-LABERINTO-DEL-FAUNO-2006.mp4?rlkey=z6e401d7q82cxqd4drps9rb4r&st=llug0ayq&dl=0"}, 
-  {name: "🍿", url: ""}, 
+  {name: "🍿Sexto Sentido", url: "https://www.dropbox.com/scl/fi/qh6xtfxzae03sihv7b6c9/SEXTO-SENTIDO.mp4?rlkey=l057f8364ghr0hthbbzwbo6tt&st=vvh4aptv&dl=0"}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""},  
   {name: "🍿", url: ""}, 
