9b446be83c57890f5b9027033f19b97a1daa1666
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-31 20:52:38 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -109,7 +109,7 @@ var Lista1 = [
   {name: "🍿Alicia Atraves Del Espejo", url: "https://www.dropbox.com/scl/fi/5hanae80xew92ukghind1/ALICIA-A-TRAV-S-DEL-ESPEJO.mp4?rlkey=u4ir0suyn5komggck33n302t9&st=klwhgii7&dl=0"}, 
   {name: "🍿Alicia En El Pais De Las Maravillas", url: "https://www.dropbox.com/scl/fi/n3e6m230j35dcnrx6jf07/Alicia-en-el-Pa-s-de-las-Maravillas.mp4?rlkey=brgmz7rkts96kl0lxwx21qqmm&st=o7k1z3hk&dl=0"},  
   { name: "🍿Van Helsing", url: "https://www.dropbox.com/scl/fi/1dkjmite5nbqlnsc95zmy/VAN-HELSING-CAZADOR-DE-MONSTRUOS.mp4?rlkey=l7j0o6rbjgg9jung3jhymqsdm&st=43citb0l&dl=0" }, 
-  {name: "🍿", url: ""}, 
+  {name: "🍿72 Horas", url: "https://www.dropbox.com/scl/fi/9iptu35v1dw8zr2vchxee/72-HORAS-2026.mp4?rlkey=tzheyldtqugw7ewl2ctl5fbfo&st=cfhao48h&dl=0"}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
   {name: "🍿", url: ""}, 
