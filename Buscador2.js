c3bea1904b57c6de2e16d9c92644f129697be116
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-08-21 19:20:19 -0400

    Add files via upload

--- Buscador2.js
+++ Buscador2.js
@@ -528,7 +528,8 @@ var Lista2 = [
     { NAME: "🌐MINIONS & MONSTRUOS", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}, 
     { NAME: "🌐TOY STORY 5", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}, 
     { NAME: "🌐PROYECTO FIN DEL MUNDO", URL: "https://lamovie.github.io/PRE-ESTRENOS/MAS.html"}, 
-    { NAME: "🌐EL FINAL DE LA CALLE OAK", URL: "https://lamovie.github.io/PRE-ESTRENOS/"} 
+    { NAME: "🌐EL FINAL DE LA CALLE OAK", URL: "https://lamovie.github.io/PRE-ESTRENOS/"}, 
+    { NAME: "🌐INSIDIOUS FUERA DEL MAS ALLÁ", URL: "https://lamovie.github.io/PRE-ESTRENOS/"} 
 ];
 
 
